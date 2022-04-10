84d9c7d753a7f72dff3c75aa1f6e1bba15dcb920
ArthurAAM <arthur.azevedo.maia@gmail.com>
2022-04-10 12:09:03 -0300

    feat: initialization of matrix and array

--- EP1/main.py
+++ EP1/main.py
@@ -1,5 +1,49 @@
+import numpy as np
+
+
+def initializeResults(n):
+    arr = []
+    for position in range(1, n + 1):
+        arr.append(np.cos((2 * np.pi * position**2)/n**2))
+    return np.array(arr)
+
+
+def calculateValue(i, j, n):
+    if i == j:
+        return 2
+
+    if i != n:
+        if i == j + 1:
+            return (2*i - 1)/(4*i)
+        if i == j - 1:
+            return 1 - (2*i - 1)/(4*i)
+
+    if i == j + 1:
+        return (2*i - 1)/(2*i)
+    if i == j - 1:
+        return 1 - (2*i - 1)/(2*i)
+    return 0
+
+
+def inicializeMatrix(n):
+    matrix = []
+    for row in range(1, n + 1):
+        line = []
+        # O enunciado trata as linhas e colunas como indo de 1 a n, enquanto o Python as trata como indo
+        # de 0 a n - 1. Isso pode gerar algumas confusões, mas é o motivo pelo qual o range, por exemplo,
+        # vai de 1 a n + 1 neste código.
+        for column in range(1, n + 1):
+            value = calculateValue(row, column, n)
+            line.append(value)
+        matrix.append(line)
+    matrix[0][-1] = 0.25
+    matrix[n - 1][0] = (1 - (2*n - 1)/2*n)
+    return np.matrix(matrix)
+
+
 def main():
-    print("vamo comecar isso aqui")
+    # print(inicializeMatrix(20))
+    print(initializeResults(20))
 
 
 if __name__ == "__main__":
