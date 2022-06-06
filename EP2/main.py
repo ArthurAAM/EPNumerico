dd2990ab98ea3ba2f9ee07cc1757741c1cb29314
ArthurAAM <arthur.azevedo.maia@gmail.com>
2022-06-05 23:36:45 -0300

    fix: minor changes

--- EP2/main.py
+++ EP2/main.py
@@ -128,7 +128,7 @@ def main():
 
         if (choice == 0):
             print("Programa encerrado.", "\n")
-            break
+            return
 
         print("Seguem as opções novamente: \n 1) Calcular o volume do cubo cujas arestas tem comprimento 1 e o do tetraedro com vértices em (0,0,0), (1,0,0), (0,1,0), (0,0,1) \n 2) Calcular a área da região no primeiro quadrante limitada pelos eixos e pela curva y = 1 - x^2 \n 3) Calcular a área e volume abaixo da superfície descrita por z = e^(x/y), 0.1 <= x <= 0.5, x^3 <= y <= x^2 \n 4) Calcular o volume da calota esférica de altura 1/4 da esfera de raio 1 e o do sólido obtido da rotação da região, em torno do eixo y, delimitada por x = 0, x = e^(-y^2), y = -1 e y = 1 \n 0) Encerrar o programa \n")
 
